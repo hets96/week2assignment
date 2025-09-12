3f182baa8e3ba7abfa43f595798d7b52e2130f74
Hetal Shah <ducksareyellowish@gmail.com>
2025-09-12 16:32:19 +0100

    save

---
+++ app.js
@@ -0,0 +1,13 @@
+const thumbnails = document.getElementById('thumbnails')
+const displayContainer = document.getElementById('displayContainer')
+
+const images = [
+    {
+        src: './images/destiny1.jpg',
+        alt: 'The Black Fleet from Destiny 2'
+    },
+    {
+        src: './images/destiny2.jpg',
+        alt: ''
+    }
+]
